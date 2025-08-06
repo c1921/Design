f0553c55437178b5cd4d11a630a111340f37368f
C1921 <w915504817@gmail.com>
2025-08-06 17:52:05 +0800

    新增：初始提交

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+onMounted(() => {
+  setTimeout(() => window.HSStaticMethods.autoInit(), 100)
+});
+</script>
+
+<template>
+
+</template>
+
