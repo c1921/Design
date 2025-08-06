285936f4ca09af552f266f0564ee9dc37e3d0101
C1921 <w915504817@gmail.com>
2025-08-06 18:08:52 +0800

    新增：创建 Category 组件并更新 App 组件以使用该组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import Category from './components/Category.vue';
 onMounted(() => {
   setTimeout(() => window.HSStaticMethods.autoInit(), 100)
 });
 </script>
 
 <template>
+  <header class="pt-15">
+    <h1 class="text-center text-7xl font-bold">MAIN TITLE</h1>
+  </header>
 
+  <main class="h-screen">
+    <div class="container mx-auto px-4">
+      <div class="grid grid-cols-1 gap-4">
+        <Category title="Category 1" description="Description for category 1"
+          image="https://dummyimage.com/600x400/000000/fff" />
+        <Category title="Category 2" description="Description for category 2"
+          image="https://dummyimage.com/600x400/000000/fff" />
+        <Category title="Category 3" description="Description for category 3"
+          image="https://dummyimage.com/600x400/000000/fff" />
+      </div>
+
+    </div>
+  </main>
 </template>
-
